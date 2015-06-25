b632871e7850912be94188ebc5f3f23a9189b3f0
laursuom <laur1.suomalainen@gmail.com>
2015-06-25 15:25:49 +0300

    did some excercises

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,21 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [xsum (+ x x)]
+     (Math/pow xsum xsum)
+  )
+  )
 
 (defn spiff [v]
-  :-)
+  (+ (get v 0) (get v 2))
+  )
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
 (defn spiff-destructuring [v]
-  :-)
+  (let[ [x y z] v] (+ x z))
+  )
 
 (defn point [x y]
   [x y])
@@ -19,19 +24,31 @@
   [bottom-left top-right])
 
 (defn width [rectangle]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle]
+    (- x2 x1)
+  )
+)
 
 (defn height [rectangle]
-  :-)
+   (let [[[x1 y1] [x2 y2]] rectangle]
+    (- y2 y1)
+  )
+)
 
 (defn square? [rectangle]
-  :-)
+  (if (== (width (rectangle)) (height (rectangle))) true false)
+)
 
 (defn area [rectangle]
-  :-)
+  (* (width (rectangle)) (height (rectangle)))
+  )
 
 (defn contains-point? [rectangle point]
-  :-)
+  (let [[[x1 y1] [x2 y2]] rectangle
+    [pointx pointy] point]
+  (if ((<= x1 pointx x2) and (<= y1 pointy y2)) true false)
+       )
+  )
 
 (defn contains-rectangle? [outer inner]
   :-)
